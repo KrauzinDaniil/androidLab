d520b5fb5a452c94a5f402028ecec52adc3d8d30
KrauzinDaniil <krauzin1995@gmail.com>
2023-11-02 13:43:33 +0600

    Final option

---
+++ app/src/main/java/daniel/Labs/androidLab/ui/theme/font.kt
@@ -0,0 +1,15 @@
+package com.example.androidLab.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.myapplication.R
+
+
+internal val FontFamily = FontFamily (
+        Font(R.font.sk_modernist_regular),
+        Font(R.font.sk_modernist_mono_res, weight = FontWeight.Medium),
+        Font(R.font.sk_modernist_bold_res, weight = FontWeight.Bold),
+
+
+        )
---
+++ app/src/main/java/daniel/Labs/androidLab/userUi.kt
@@ -0,0 +1,7 @@
+package com.example.androidLab
+
+data class userUi(val text: Int, val date: Int, val user : user ){
+
+
+}
+class user(val username: Int, val image: Int);
